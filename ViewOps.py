630ed5cac4eef3d08ba40052c7ed0596111910cd
Terri <tmgast@gmail.com>
2022-01-30 22:18:49 +0900

    saving work before scrapping viewport split code.

--- ViewOps.py
+++ ViewOps.py
@@ -8,28 +8,8 @@ import bpy
 import math
 from mathutils import Vector
 import gpu
-from bgl import *
-from gpu_extras.batch import batch_for_shader
-from gpu_extras.presets import draw_circle_2d
-from bpy.app.handlers import persistent
-
-class View3DPanel:
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "ViewOps"
-    
-class PanelOne(View3DPanel, bpy.types.Panel):
-    bl_idname = "VIEW3D_PT_view_ops"
-    bl_label = "Viewport Settings"
-
-    def draw(self, context):
-        self.layout.label(text="Control Zones")
-        row = self.layout.row()
-        self.layout.prop(context.window_manager, "dolly_wid")
-        self.layout.prop(context.window_manager, "pan_rad")
-        row = self.layout.row()
-        self.layout.prop(context.window_manager, "con_overlay")
-    
+import bgl
+from gpu_extras.batch import batch_for_shader    
 
 class TouchInput(bpy.types.Operator):
     bl_idname = "view3d.view_ops"
@@ -57,8 +37,8 @@ class TouchInput(bpy.types.Operator):
     def invoke(self, context, event):
         self.delta = Vector((event.mouse_region_x, event.mouse_region_y))
         mid_point = self.getMidPoint(context.area)
-        dolly_scale = context.window_manager.dolly_wid
-        pan_scale = context.window_manager.pan_rad
+        dolly_scale = context.area.ov.dolly_wid
+        pan_scale = context.area.ov.pan_rad
         
         dolly_wid = mid_point.x * dolly_scale
         pan_diameter = math.dist((0,0), mid_point) * (pan_scale*0.4)
@@ -92,66 +72,144 @@ class TouchInput(bpy.types.Operator):
 # import gpu
 # from bgl import *
 # from gpu_extras.batch import batch_for_shader
-def update_overlay(self, context):
-    if self == "NONE": self = overlay_manager
-    if context.area == None: context = bpy.context
-    clear_overlays(self, context)
-    wm = context.window_manager
-    if wm.con_overlay:
-        dolly_scale = wm.dolly_wid
-        view_width = context.area.width
-        view_half_wd = view_width/2
-        view_half_ht = context.area.height/2
-        pan_diameter = math.dist((0,0), (view_half_wd, view_half_ht)) * (wm.pan_rad*0.4)
+
+###
+### NEED REWRITE TO USE TIMER AND UPDATE SCREEN AREAS
+###    ONLY IF SHOW OVERLAY IS ON
+###
+class Overlay:
+    def __init__(self, area):
+        self.area = area
+        self.pointer = str(area.as_pointer())
+        overlays = []
+    
+        wm = bpy.types.WindowManager.viewops_conf
+        wm[self.pointer+"dolly_wid"]: bpy.props.FloatProperty(
+            name="Rail Width", 
+            default=0.4, 
+            min=0.1, 
+            max=1
+        )
+        wm[self.pointer+"pan_rad"]: bpy.props.FloatProperty(
+            name="Center Radius", 
+            default=0.35, 
+            min=0.1, 
+            max=1.0
+        )
+        wm[self.pointer+"isVisible"]: bpy.props.BoolProperty(
+            name="Show Overlay", 
+            default=False
+        )
         
-        left_rail = (
-            Vector((0.0,0.0)), 
-            Vector((view_width/2*dolly_scale, context.area.height))
-        )
-        right_rail = (
-            Vector((view_width,0.0)), 
-            Vector((view_width - view_width/2*dolly_scale, context.area.height))
-        )
-        mid_ring = (
-            Vector((view_half_wd, view_half_ht)),
-            pan_diameter
-        )
-        overlay_manager.renderShape(
-            "left_rail",
-            "RECT",
-            left_rail,
-            (0,0.5,0.5,0.10)
-        )
-        overlay_manager.renderShape(
-            "right_rail",
-            "RECT",
-            right_rail,
-            (0,0.5,0.5,0.10)
-        )
-        overlay_manager.renderShape(
-            "mid_ring",
-            "CIRC",
-            mid_ring,
-            (00.5,0.2,0.2,0.10)
-        )
-            
-def clear_overlays(self, context):
-    overlays = ["left_rail","right_rail","mid_ring"]
-    for ov in overlays:
-        overlay_manager.clearOverlay(ov)
+    def get(self, attr):
+        wm = bpy.types.WindowManager.viewops_conf
+        return wm[self.pointer+attr]
+
+    def add_overlay(self, overlay):
+        self.overlays.append(overlay)
+
+    def clear_overlays(self):
+        for ol in self.overlays:
+            bpy.types.SpaceView3D.draw_handler_remove(ol, 'WINDOW')
+        self.overlays = []
 
 class OverlayAgent:
     def __init__(self):
-        self.objects = []
-        bpy.app.timers.register(self.run_10_times)
-
-    def run_10_times(self):
-        counter=0
-        if counter == 10:
-            return None
+        bpy.app.timers.register(self.refresh_overlays)
+        self.views = []
         
-        update_overlay(self, bpy.context)
-        return 0.1
+    def indexView(self, area):
+        ao = (area, Overlay(area))
+        self.views.append(ao)
+        return ao
+    
+    def findView(self, view):
+        for area, overlay in self.views:
+            if area == view: return (area, overlay)
+        return False
+
+    def refresh_overlays(self):
+        self.update_overlay()
+        return None
+
+    def init_viewport(self, view):
+        ao = self.findView(view)
+        if ao == False: return self.indexView(view)
+        return ao
+
+    def update_overlay(self):
+        self.clearAll()
+
+        # find/register all viewports
+        for area in bpy.context.window.screen.areas.values():
+            if area.type != "VIEW_3D": continue
+            a, ov = self.init_viewport(area)
+
+            if ov.get("isVisible"):
+                wd = area.width
+                ht = area.height
+                print(ov.get("pan_rad"))
+                pan_diameter = math.dist((0,0), (wd/2, ht/2)) * (ov.get("pan_rad") * 0.4)
+                
+                left_rail = (
+                    Vector((0.0,0.0)), 
+                    Vector((wd/2*ov.get("dolly_wid"), ht))
+                )
+                right_rail = (
+                    Vector((wd,0.0)), 
+                    Vector((wd - wd/2*ov.get("dolly_wid"), ht))
+                )
+                mid_ring = (
+                    Vector((wd/2, ht/2)),
+                    pan_diameter
+                )
+                area.ov.add_overlay(
+                    overlay_manager.renderShape(
+                        "left_rail", "RECT", left_rail, (0,0.5,0.5,0.10)
+                ))
+                area.ov.add_overlay(
+                    overlay_manager.renderShape(
+                        "right_rail",
+                        "RECT",
+                        right_rail,
+                        (0,0.5,0.5,0.10)
+                ))
+                area.ov.add_overlay(
+                    overlay_manager.renderShape(
+                        "mid_ring",
+                        "CIRC",
+                        mid_ring,
+                        (00.5,0.2,0.2,0.10)
+                ))
+
+    def clearAll(self):
+        for area in bpy.context.window.screen.areas.values():
+            if area.type != "VIEW_3D": continue
+            if not hasattr(area.spaces.data, "ov"): continue
+            area.spaces.data.ov.clear_overlays()
+            area.tag_redraw()
+
+    def renderShape(self, name, shape, args, color):
+        # create draw call
+        if shape == "CIRC":
+            _handle = bpy.types.SpaceView3D.draw_handler_add(
+                self.drawVectorCircle, 
+                (args[0],args[1], color), 
+                'WINDOW', 
+                'POST_PIXEL'
+            )
+        elif shape == "RECT":
+            _handle = bpy.types.SpaceView3D.draw_handler_add(
+                self.drawVectorBox, 
+                (args[0],args[1], color), 
+                'WINDOW', 
+                'POST_PIXEL'
+            )
+        else:
+            return {"INVALID_SHAPE"}
+            
+        bpy.context.area.tag_redraw()
+        return _handle
 
     def drawVectorBox(self, a, b, color):
         vertices = (
@@ -167,11 +225,12 @@ class OverlayAgent:
         shader.uniform_float("color", color)
         batch.draw(shader)
         glDisable(GL_BLEND)
-    
+        
     def drawVectorCircle(self, mid, rad, color):
         segments = 100
         vertices = [Vector(mid)]
         indices = []
+        p = 0
         for p in range(segments):
             if p > 0:
                 point = Vector((
@@ -190,53 +249,7 @@ class OverlayAgent:
         batch.draw(shader)
         glDisable(GL_BLEND)
 
-
-    def renderShape(self, name, shape, args, color):
-        # create draw call
-        if shape == "RECT":
-            _handle = bpy.types.SpaceView3D.draw_handler_add(
-                self.drawVectorBox, 
-                (args[0],args[1], color), 
-                'WINDOW', 
-                'POST_PIXEL'
-            )
-        elif shape == "CIRC":
-            _handle = bpy.types.SpaceView3D.draw_handler_add(
-                self.drawVectorCircle, 
-                (args[0],args[1], color), 
-                'WINDOW', 
-                'POST_PIXEL'
-            )
-            
-        bpy.context.area.tag_redraw()
-        self.objects.append((name, _handle))
-        
-        # remove draw call and clear objects
-    def clearOverlay(self, name):
-        # search for overlay
-        _handlers = [item for item in self.objects if item[0] == name]
-
-        #remove if found
-        for element in _handlers:
-            bpy.types.SpaceView3D.draw_handler_remove(element[1], 'WINDOW')
-            self.objects.remove(element)
-            
-        viewports = []
-        for area in bpy.context.window.screen.areas:
-            if area.type == "VIEW_3D":
-                viewports.append(area)
-    
-    def clearAll(self):
-        for element in self.objects:
-            bpy.types.SpaceView3D.draw_handler_remove(element[1], 'WINDOW')
-            bpy.context.area.tag_redraw()
-            self.objects.remove(element)
-            
-@persistent
-def handle_vp_change(self, context, *args):
-    print(args[0])
-    
-
+overlay_manager = OverlayAgent()
 
     ## NEED TO TEST
 #@persistent
@@ -250,31 +263,32 @@ def handle_vp_change(self, context, *args):
 ### END DRAW SCRIPT
 #########################################
 
-overlay_manager = OverlayAgent()
+class View3DPanel:
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "ViewOps"
+    
+class PanelOne(View3DPanel, bpy.types.Panel):
+    bl_idname = "VIEW3D_PT_view_ops"
+    bl_label = "Viewport Settings"
+
+    def draw(self, context):
+        overlay_manager.update_overlay()
+        a, ov = overlay_manager.findView(context.area)
+        self.layout.label(text="Control Zones")
+        row = self.layout.row()
+        self.layout.prop(a.spaces.data, "dolly_wid")
+        row = self.layout.row()
+        self.layout.prop(a.spaces.data, "pan_rad")
+        row = self.layout.row()
+        self.layout.prop(a.spaces.data, "isVisible", text="Show Overlay")
+
 addon_keymaps = []
 
 def register():
-    wm = bpy.context.window_manager    
+    wm = bpy.context.window_manager   
+    bpy.types.WindowManager.viewops_conf = {}
     bpy.utils.register_class(TouchInput)
-    bpy.types.WindowManager.dolly_wid = bpy.props.FloatProperty(
-        name="Rail Width", 
-        default=0.4, 
-        min=0.1, 
-        max=1,
-        update=update_overlay
-    )
-    bpy.types.WindowManager.pan_rad = bpy.props.FloatProperty(
-        name="Center Radius", 
-        default=0.35, 
-        min=0.1, 
-        max=1.0,
-        update=update_overlay
-    )
-    bpy.types.WindowManager.con_overlay = bpy.props.BoolProperty(
-        name="Show Overlay", 
-        default=False,
-        update=update_overlay
-    )
 
 #    bpy.app.handlers.load_post.append(load_handler)
     bpy.utils.register_class(PanelOne)
---
+++ map.md
@@ -0,0 +1,6 @@
+Area
+- VO_isVisible
+- VO_rail_width
+- VO_pan_radius
+- VO_Overlay
+  - handlers
